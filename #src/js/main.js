9d546b173995248b397e3a72895099c45fff6715
dima-gdp <dima5620g@gmail.com>
2020-11-18 09:55:53 +0300

    added sliders

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
-	const slider_1 = new Swiper('.tabs__body', {
+	const slider_1 = new Swiper('.slider-1', {
 
 		slidesPerView: 4,
-		spaceBetween: 30,
+		spaceBetween: 31,
 		observer: true,
 		observeParents: true,
-		observeSlideChildren: true,
-		loop: false,
-		// navigation: {
-		// 	nextEl: '.swiper-button-next',
-		// 	prevEl: '.swiper-button-prev',
-		// },
+		observeSlideChildren: false,
+		loop: true,
+		navigation: {
+			nextEl: '.slider-1-next',
+			prevEl: '.slider-1-prev',
+		},
 	});
 
 	const slider_2 = new Swiper('.slider-2', {
@@ -20,7 +20,7 @@ $(document).ready(function () {
 		spaceBetween: 31,
 		// observer: true,
 		// observeParents: true,
-		loop: false,
+		loop: true,
 		navigation: {
 			nextEl: '.slider-2__next',
 			prevEl: '.slider-2__prev',
@@ -30,7 +30,7 @@ $(document).ready(function () {
 
 	$('ul.tabs__items').on('click', 'li:not(.active)', function () {
 		$(this).addClass('active').siblings().removeClass('active')
-			.closest('div.tabs').find('div.tabs__block').removeClass('swaper-wrapper active').eq($(this).index()).addClass('active swaper-wrapper');
+			.closest('div.tabs').find('div.tabs__block').removeClass('active').eq($(this).index()).addClass('active');
 	})
 
 
@@ -40,25 +40,46 @@ $(document).ready(function () {
 	ymaps.ready(init);
 
 	function init() {
-		// Создание карты.
 		var myMap = new ymaps.Map("map", {
 			center: [43.1420542527047, 131.91734209167475],
-			zoom: 14,
+			zoom: 14
+		}, {
+			searchControlProvider: 'yandex#search'
 		});
 
-		var point = new ymaps.GeoObject({
-			geometry: {
-				type: "Point", // тип геометрии - точка
-				coordinates: [43.1420542527047, 131.91734209167475], // координаты точки
-			}
-		},
+		// Открываем балун на карте (без привязки к геообъекту).
+		myMap.balloon.open(
+			[43.14, 131.91],
+			"График работы:<br>Пн-Вс 9:00 - 22:00",
 			{
-				iconLayout: 'default#imageWithContent',
-				iconImageHref: '../img/point-map.svg',
-				iconImageSize: [311, 170],
-			}
-		);
-
-		myMap.geoObjects.add(point);
+				// Опция: не показываем кнопку закрытия.
+				closeButton: false,
+			});
 	}
+
+
+
+
+	// function init() {
+	// 	// Создание карты.
+	// 	var myMap = new ymaps.Map("map", {
+	// 		center: [43.1420542527047, 131.91734209167475],
+	// 		zoom: 14,
+	// 	});
+
+	// 	var point = new ymaps.GeoObject({
+	// 		geometry: {
+	// 			type: "Point", // тип геометрии - точка
+	// 			coordinates: [43.1420542527047, 131.91734209167475], // координаты точки
+	// 		}
+	// 	},
+	// 		{
+	// 			iconLayout: 'default#imageWithContent',
+	// 			iconImageHref: '../img/point-map.svg',
+	// 			iconImageSize: [311, 170],
+	// 		}
+	// 	);
+
+	// 	myMap.geoObjects.add(point);
+	// }
 });
